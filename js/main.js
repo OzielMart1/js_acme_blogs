a4c5ac6e468fc2c7f58fa346b5eaa36c329b75b1
Oziel Martinez <o_martinezmondragon@mail.fhsu.edu>
2024-11-24 23:04:52 -0600

    second function

--- js/main.js
+++ js/main.js
@@ -11,3 +11,23 @@ function createElemWithText(elementType= "p", textContent= "", className= ""){
 }
 
 // 2
+
+function createSelectOptions(users){
+    if(!users){
+        return undefined;
+    }
+    const options=[];
+
+    users.forEach(user=>{
+        const option= document.createElement('option');
+
+        option.value = user.id;
+
+        option.textContent= user.name;
+
+        options.push(option);
+
+    });
+return options;
+
+}
