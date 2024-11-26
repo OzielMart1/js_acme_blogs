fac9eadf596fc4bc0e54c3390ec2eb845531179e
Oziel Martinez <o_martinezmondragon@mail.fhsu.edu>
2024-11-25 22:25:09 -0600

    commit 11/24/24 11pm

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function createElemWithText(elementType= "p", textContent= "", className= ""){
 
 // 2
 
-function createSelectOptions(users){ // this one is correct
+function createSelectOptions(users){
     if(!users){
         return undefined;
     }
@@ -122,18 +122,21 @@ function createComments(comments){
 }
 
 //9 
-function createSelectOptions(users){
-    return users.map(user => {
-        const option= document.createElement('option');
-        option.value =user.id;
-        option.textContent=user.name;
-        return option;
+function populateSelectMenu(users){
+    const selectMenu=document.getElementById('selectMenu');
+    selectMenu.innerHTML='';
+
+    const options = createSelectOptions(users);
+
+    options.forEach(option =>{
+        selectMenu.appendChild(option);
     });
+    return selectMenu;
 }
 
 //10 
 
-async function getUsers(){
+async function getUsers(){ //correct
     try{
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         if(!response.ok){
@@ -164,4 +167,200 @@ async function getUserPosts(userId){
         console.error('Error fetching posts for user:', error);
         return null;
     }
+}
+
+//12 
+async function getUser(userId){
+    try{
+        const response= await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+
+        if(!response.ok){
+            throw new Error(`User with ID ${userId} not found`);
+        }
+
+        const userData= await response.json();
+        return userData;
+
+    }catch(error){
+    console.error('Error fetching user data:', error);
+    throw error;
+    }
+}
+//13
+async function getPostComments(postId){
+    try{
+        const response= await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
+
+        if(!response.ok){
+            throw new Error(`Comments for post ID ${postId} not found`);
+
+        }
+        const commentsData= await response.json();
+        return commentsData;
+
+    }catch(error){
+        console.error('Error fetching comments: ', error);
+        throw error;
+    }
+}
+//14
+async function displayComments(postId){
+    try{
+        const section = document.createElement('section');
+        section.dataset.postId= postId;
+
+        section.classList.add('comments', 'hide');
+
+        const comments= await getPostComments(postId);
+        const fragment= createComments(comments);
+
+        section.appendChild(fragment);
+        return section;
+    }catch(error){
+        console.error('Error displaying comments: ', error);
+        throw error;
+    }
+}
+//15 
+async function createPosts(postss){
+    const fragment= document.createDocumentFragment();
+    for(const post of posts){
+        try{
+            const article = document.createElement('article');
+
+            const h2 = document.createElement('h2');
+            h2.textContent= post.title;
+            article.appendChild(h2);
+
+            const pBody=document.createElement('p');
+            pBody.textContent= post.body;
+            article.appendChild(pBody);
+
+            const pId= docment.createElement('p');
+            pId.textContent= `Post ID: ${post.id}`;
+            article.appendChild(pId);
+
+            const author = await getUser(post.userId);
+
+            const pAuthor= document.createElement('p');
+            pAuthor.textContent= `Author: ${author.name} with ${author.company.name}`;
+            article.appendChild(pAuthor);
+
+            const pCatchphrase= document.createElement('p');
+            pCatchphrase.textContent= `Company Catchphrase: "${author.company.catchPhrase}`;
+            article.appendChild(pCathcphrase);
+
+            const button = document.createElement('button');
+            button.textContent= 'Show Comments';
+            button.dataset.postId= post.id;
+            article.appendChild(button);
+
+            fragment.appendChild(article);
+        }catch(error){
+            console.error('Error processing post: ', error);
+
+            }
+
+        }
+        return fragment;
+    }
+
+//16 
+async function displayPosts(posts){
+    const main= document.querySelector('main');
+
+    const element = posts && posts.length>0
+    ? await createPosts(posts): createElemWithText('p', 'No posts available.');
+    main.appendChild(element);
+
+    return element;
+
+}
+
+//17 
+
+async function toggleComments(event,postId){
+    try{
+        event.target.listener= true;
+
+        const section = await toggleCommentSection(postId);
+
+        const button = toggleCommentButton(postId);
+
+        return [section,button];
+    }catch(error){
+        console.error('Error toggling comments: ', error);
+
+    }
+}
+//18 
+async function refreshPosts(posts){
+    try{
+        const removeButtons= removeButtonListener();
+
+        const main= document.querySelector('main');
+        const clearedMain= deleteChildElements(main);
+
+        const fragment=await displayPosts(posts);
+
+        const addButtons= addButtonListeners();
+
+        return [removeButtons, clearedMain, fragment, addButtons];
+
+    }catch(error){
+        console.error('Error refreshing posts: ', error);
+
+    }
+
+}
+
+//19
+async function selectMenuChangeEventHandler(event){
+    try{
+        const selectMenu= event.target;
+        selectMenu.disabled= true;
+
+        const userId= event.target.value|| 1;
+
+        const posts= await getUserPosts(userId);
+
+        const refreshPostsArray= await refreshPosts(posts);
+
+        selectMenu.disable = false;
+
+        return [userId, posts, refreshPostsArray];
+
+    }catch(error){
+        console.error('Error handling select menu change: ', error);
+
+    }
+}
+
+//20
+
+async function initPage(){
+    try{
+        const users= await getUsers();
+
+        const select= populateSelectMenu(users);
+
+        return [users, select];
+
+    }catch(error){
+        console.error("Error initializing the page: ", error);
+
+    }
+}
+//21 
+async function initApp(){
+    initPage().then(() => {
+        const selectMenu= document.querySelector('#user-select');
+        selectMenu.addEventListener('change', function(event){
+            selectMenuChangeEventHandler(event);
+        });
+
+    }).catch(error=>{
+        console.error("Error initializing the app: ", error);
+
+    });
 }
